605aca73b1ca2c40d0f1124a3df65317673a7f19
Yunjinho <dbswlsgh1238@naver.com>
2023-05-04 16:59:03 +0900

    Board CRUD

--- thisisboard/src/thisisboard/model/dao/BoardDao.java
+++ thisisboard/src/thisisboard/model/dao/BoardDao.java
@@ -9,12 +9,14 @@ import java.util.ArrayList;
 import thisisboard.model.BoardDataSource;
 import thisisboard.model.idao.IBoardDAO;
 import thisisboard.model.vo.BoardVo;
-import thisisboard.model.vo.MemberVo;
 
 public class BoardDao implements IBoardDAO{
 
 	@Override
 	public ArrayList<BoardVo> getAllBaordList() {
+		
+		ArrayList<BoardVo> boardList = new ArrayList<BoardVo>();
+		
 		String sql="select * from board";
 		Connection con= null;
 		PreparedStatement stmt=null;
@@ -29,41 +31,143 @@ public class BoardDao implements IBoardDAO{
 				boardvo.setBtitle(rs.getString("btitle"));
 				boardvo.setBwriter(rs.getString("bwriter"));
 				boardvo.setBdate(rs.getDate("bdate"));
+				boardvo.setBcontent(rs.getString("bcontent"));
+				boardList.add(boardvo);
 			}
 		} catch (SQLException e) {
-			// TODO: handle exception
+			throw new RuntimeException(e);
+		}finally {
+			try {rs.close();} catch (SQLException e) {}
+			try {stmt.close();} catch (SQLException e) {}
+			BoardDataSource.closeConnection(con);
 		}
-				
-		return null;
+		return boardList;
 	}
 
 	@Override
-	public ArrayList<BoardVo> getMyBoardList(MemberVo memberVo) {
-		return null;
+	public ArrayList<BoardVo> getMyBoardList(String userid) {
+		ArrayList<BoardVo> boardList = new ArrayList<BoardVo>();
+		String sql="select * from board where bwriter=?";
+		Connection con= null;
+		PreparedStatement stmt=null;
+		ResultSet rs=null;
+		try {
+			con=BoardDataSource.getConnection();
+			stmt=con.prepareStatement(sql);
+			stmt.setString(1, userid);
+			rs=stmt.executeQuery();
+			while(rs.next()) {
+				BoardVo boardvo=new BoardVo();
+				boardvo.setBno(rs.getInt("bno"));
+				boardvo.setBtitle(rs.getString("btitle"));
+				boardvo.setBwriter(rs.getString("bwriter"));
+				boardvo.setBdate(rs.getDate("bdate"));
+				boardvo.setBcontent(rs.getString("bcontent"));
+				boardList.add(boardvo);
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}finally {
+			try {rs.close();} catch (SQLException e) {}
+			try {stmt.close();} catch (SQLException e) {}
+			BoardDataSource.closeConnection(con);
+		}
+		return boardList;
 	}
 
 	@Override
-	public int deleteBoard(int dno) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int deleteBoard(int bno) {
+		int rowCount=0;
+		String sql="delete from board where bno=?";
+		Connection con= null;
+		PreparedStatement stmt=null;
+		ResultSet rs=null;
+		try {
+			con=BoardDataSource.getConnection();
+			stmt=con.prepareStatement(sql);
+			stmt.setInt(1, bno);
+			rowCount=stmt.executeUpdate();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}finally {
+			try {rs.close();} catch (SQLException e) {}
+			try {stmt.close();} catch (SQLException e) {}
+			BoardDataSource.closeConnection(con);
+		}
+		return rowCount;
 	}
 
 	@Override
 	public BoardVo getMyBoard(int dno) {
-		// TODO Auto-generated method stub
-		return null;
+		BoardVo boardvo=new BoardVo();
+		String sql="select * from board where bno=?";
+		Connection con= null;
+		PreparedStatement stmt=null;
+		ResultSet rs=null;
+		try {
+			con=BoardDataSource.getConnection();
+			stmt=con.prepareStatement(sql);
+			stmt.setInt(1, dno);
+			rs=stmt.executeQuery();
+			while(rs.next()) {
+				boardvo.setBno(rs.getInt("bno"));
+				boardvo.setBtitle(rs.getString("btitle"));
+				boardvo.setBwriter(rs.getString("bwriter"));
+				boardvo.setBdate(rs.getDate("bdate"));
+				boardvo.setBcontent(rs.getString("bcontent"));
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}finally {
+			try {rs.close();} catch (SQLException e) {}
+			try {stmt.close();} catch (SQLException e) {}
+			BoardDataSource.closeConnection(con);
+		}
+		return boardvo;
 	}
 
 	@Override
-	public int updateMyBoard(int dno) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int updateMyBoard(BoardVo boardvo) {
+		int rowCount=0;
+		String sql="update board set btitle=?,bcontent=? where bno=?";
+		Connection con= null;
+		PreparedStatement stmt=null;
+		try {
+			con=BoardDataSource.getConnection();
+			stmt=con.prepareStatement(sql);
+			stmt.setString(1, boardvo.getBtitle());
+			stmt.setString(2, boardvo.getBcontent());
+			stmt.setInt(3, boardvo.getBno());
+			rowCount=stmt.executeUpdate();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}finally {
+			try {stmt.close();} catch (SQLException e) {}
+			BoardDataSource.closeConnection(con);
+		}
+		return rowCount;
 	}
 
 	@Override
-	public int insertMyboard(BoardVo boardVo) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int insertMyboard(BoardVo boardvo) {
+		int rowCount=0;
+		String sql="insert into board (bno,btitle,bcontent,bwriter,bdate) values (SEQ_BNO.NEXTVAL,?,?,?,sysdate)";
+		Connection con= null;
+		PreparedStatement stmt=null;
+		try {
+			con=BoardDataSource.getConnection();
+			stmt=con.prepareStatement(sql);
+			stmt.setString(1, boardvo.getBtitle());
+			stmt.setString(2, boardvo.getBcontent());
+			stmt.setString(3, boardvo.getBwriter());
+			rowCount=stmt.executeUpdate();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}finally {
+			try {stmt.close();} catch (SQLException e) {}
+			BoardDataSource.closeConnection(con);
+		}
+		return rowCount;
 	}
 
 }
--- thisisboard/src/thisisboard/model/idao/IBoardDAO.java
+++ thisisboard/src/thisisboard/model/idao/IBoardDAO.java
@@ -7,11 +7,11 @@ import thisisboard.model.vo.MemberVo;
 
 public interface IBoardDAO {
 	ArrayList<BoardVo> getAllBaordList();//전체 글 목록
-	ArrayList<BoardVo> getMyBoardList(MemberVo memberVo);// 내가 쓴 글 목록
+	ArrayList<BoardVo> getMyBoardList(String userid);// 내가 쓴 글 목록
 	
-	int deleteBoard(int dno);// 삭제
-	BoardVo getMyBoard(int dno);//하나 선택
-	int updateMyBoard(int dno);//선택된 게시슬 수정
+	int deleteBoard(int bno);// 삭제
+	BoardVo getMyBoard(int bno);//하나 선택
+	int updateMyBoard(BoardVo boardVo);//선택된 게시슬 수정
 	int insertMyboard(BoardVo boardVo);//게시글 입력
 	
 
