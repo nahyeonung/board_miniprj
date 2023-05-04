05ef56f5ff383fbd3bb5c223036392a12f913f4e
wxxk <dwde22@gmail.com>
2023-05-04 17:04:35 +0900

    member insert update delete

--- thisisboard/src/thisisboard/model/vo/MemberVo.java
+++ thisisboard/src/thisisboard/model/vo/MemberVo.java
@@ -1,5 +1,18 @@
 package thisisboard.model.vo;
 
-public class MemberVo {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class MemberVo {
+	private String userid;
+	private String username;
+	private String userpassword;
 }
