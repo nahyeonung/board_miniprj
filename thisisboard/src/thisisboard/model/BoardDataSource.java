bed1c85b2cea4c5168acaa6dd59e7859ec84779b
wxxk <dwde22@gmail.com>
2023-05-04 15:32:23 +0900

    라이브러리

--- thisisboard/src/thisisboard/model/BoardDataSource.java
+++ thisisboard/src/thisisboard/model/BoardDataSource.java
@@ -1,5 +1,7 @@
 package thisisboard.model;
 
 public class BoardDataSource {
-
+	
+	private static final String DRIVER = "oracle.jdbc.OracleDriver";
+	private static final String URL = "jdbc:orale:thin:@192.168.0.145:154"
 }
