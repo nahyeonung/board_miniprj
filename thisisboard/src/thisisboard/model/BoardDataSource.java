37c87d5126987f432e5cea46559141ccd969c446
wxxk <dwde22@gmail.com>
2023-05-04 15:34:23 +0900

    datasource

--- thisisboard/src/thisisboard/model/BoardDataSource.java
+++ thisisboard/src/thisisboard/model/BoardDataSource.java
@@ -1,7 +1,43 @@
 package thisisboard.model;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
 public class BoardDataSource {
 	
 	private static final String DRIVER = "oracle.jdbc.OracleDriver";
-	private static final String URL = "jdbc:orale:thin:@192.168.0.145:154"
+	private static final String URL = "jdbc:orale:thin:@192.168.0.145:1521/xe";
+	private static final String USERNAME = "hr";
+	private static final String PASSWORD = "hr";
+	
+	private static BasicDataSource dataSource;
+	
+	static {
+		try {
+			dataSource = new BasicDataSource();
+			dataSource.setDriverClassName(DRIVER);            
+			dataSource.setUrl(URL);
+			dataSource.setUsername(USERNAME);                                  
+			dataSource.setPassword(PASSWORD);  
+			dataSource.setInitialSize(10);
+			dataSource.setMaxTotal(10);
+			System.out.println("DataSource created");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public static Connection getConnection() throws SQLException {
+		return dataSource.getConnection();
+	}
+
+	public static void closeConnection(Connection con) {
+		if(con!=null) {
+			try {
+				con.close();
+			}catch(Exception e) {}
+		}
+	}
 }
